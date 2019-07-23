939116c02443c70e933e4c85cc8009558035813f
Marcio Junior <marciofmjr@hotmail.com>
2019-07-23 14:34:03 -0300

    Adds validation to not apply a discount bigger than available

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -11,8 +11,17 @@ class Cammino_Loyalty_PaymentController extends Mage_Core_Controller_Front_Actio
                 Mage::helper("loyalty")->resetLoyaltyDiscount();
                 $this->respond(true, "discount desabled successfully");
             } else {
-                Mage::helper("loyalty")->setLoyaltyDiscount($discount);
-                $this->respond(true, "discount applied successfully");
+
+                // Verifica se o cliente tem esse valor disponivel para utilizar
+                $availablePoints = Mage::getModel("loyalty/points")->getAvailablePoints();
+                $availableDiscount = Mage::helper("loyalty")->calcDiscount($availablePoints);
+
+                if($discount <= $availableDiscount) {
+                    Mage::helper("loyalty")->setLoyaltyDiscount($discount);
+                    $this->respond(true, "discount applied successfully");
+                } else {
+                    $this->respond(false, "trying to apply bigger discount than available");
+                }
             }
         }
     }
