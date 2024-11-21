2bdc572ecfff8b89aff85a47e3d683313709381e
ArthurMenegoli <arthur.menegoli@gmail.com>
2024-11-21 05:13:13 -0300

    DEV-2317 - Envelop tax collect in a try catch to ensure payment method instance

--- Model/Sales/Quote/Address/Total/Tax.php
+++ Model/Sales/Quote/Address/Total/Tax.php
@@ -1,17 +1,19 @@
 <?php
-class Cammino_Loyalty_Model_Sales_Quote_Address_Total_Tax extends Mage_Sales_Model_Quote_Address_Total_Abstract {
+class Cammino_Loyalty_Model_Sales_Quote_Address_Total_Tax extends Mage_Sales_Model_Quote_Address_Total_Abstract
+{
 
     protected $_code = 'loyalty_discount';
-  
-    public function collect(Mage_Sales_Model_Quote_Address $address) {
+
+    public function collect(Mage_Sales_Model_Quote_Address $address)
+    {
         parent::collect($address);
 
         $helper = Mage::helper("loyalty");
 
         if (!count($this->_getAddressItems($address)))
             return $this;
-        
-        if(Mage::helper("loyalty")->hasLoyaltyDiscountApplied()) {
+
+        if (Mage::helper("loyalty")->hasLoyaltyDiscountApplied()) {
             $discount = -Mage::helper("loyalty")->getLoyaltyDiscount(); //1990
         } else {
             $discount = 0;
@@ -19,43 +21,52 @@ class Cammino_Loyalty_Model_Sales_Quote_Address_Total_Tax extends Mage_Sales_Mod
 
         $quote = $address->getQuote($discount);
 
-        if (($discount < 0) && (strpos($helper->getDiscountPaymentMethods(), $quote->getPayment()->getMethodInstance()->getCode()) !== false)) {
-            $totals = Mage::getSingleton('checkout/session')->getQuote()->getTotals();
-            $subTotal = $totals["subtotal"]->getValue();
-            $shipping = 0;
-            if(!empty($totals["shipping"])) {
-                $shipping = $totals["shipping"]->getValue();
+        try {
+            $methodCode = '';
+            if ((!empty($quote->getPayment())) && (!empty($quote->getPayment()->getMethodInstance()))) {
+                $methodCode = $quote->getPayment()->getMethodInstance()->getCode();
             }
-            $subTotal = $subTotal + $shipping;
-            $subTotalWithLoyalty = $subTotal + $discount;
-            $paymentMethodDiscount = ($helper->getDiscountPercentage() / 100) * $subTotalWithLoyalty;
-            $discount = $discount - $paymentMethodDiscount;
-            Mage::getSingleton('core/session')->setLoyaltyPaymentMethodDiscount(true);
-        } else {
-            Mage::getSingleton('core/session')->setLoyaltyPaymentMethodDiscount(false);
+            if (($discount < 0) && (strpos($helper->getDiscountPaymentMethods(), $methodCode) !== false)) {
+                $totals = Mage::getSingleton('checkout/session')->getQuote()->getTotals();
+                $subTotal = $totals["subtotal"]->getValue();
+                $shipping = 0;
+                if (!empty($totals["shipping"])) {
+                    $shipping = $totals["shipping"]->getValue();
+                }
+                $subTotal = $subTotal + $shipping;
+                $subTotalWithLoyalty = $subTotal + $discount;
+                $paymentMethodDiscount = ($helper->getDiscountPercentage() / 100) * $subTotalWithLoyalty;
+                $discount = $discount - $paymentMethodDiscount;
+                Mage::getSingleton('core/session')->setLoyaltyPaymentMethodDiscount(true);
+            } else {
+                Mage::getSingleton('core/session')->setLoyaltyPaymentMethodDiscount(false);
+            }
+
+
+            $quote->setLoyaltytax($discount);
+            $quote->setBaseLoyaltytax($discount);
+
+            $address->setGrandTotal($discount);
+            $address->setBaseGrandTotal($discount);
+
+            $address->setWalletDebit($discount);
+            $address->setBaseWalletDebit($discount);
+
+            return $this;
+        } catch (Exception $e) {
+            return $this;
         }
-        
-
-        $quote->setLoyaltytax($discount);
-        $quote->setBaseLoyaltytax($discount);
-
-        $address->setGrandTotal($discount);
-        $address->setBaseGrandTotal($discount);
-
-        $address->setWalletDebit($discount);
-        $address->setBaseWalletDebit($discount);
-
-        return $this;
     }
 
-  
-    public function fetch(Mage_Sales_Model_Quote_Address $address) {
+
+    public function fetch(Mage_Sales_Model_Quote_Address $address)
+    {
         $helper = Mage::helper("loyalty");
-        if($helper->hasLoyaltyDiscountApplied()) {
+        if ($helper->hasLoyaltyDiscountApplied()) {
             $tax = $address->getWalletDebit();
 
             $title = '';
-            
+
             if (!empty(Mage::getStoreConfig('loyalty/advanced/show_points'))) {
                 $title = 'Resgate de Pontos';
             } else {
